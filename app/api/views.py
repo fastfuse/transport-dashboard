7480513d6b769634b89e5d0688ad5ac40a0fc81d
fastfuse <banderstadt251@gmail.com>
2018-06-09 20:42:48 +0300

    gitignore updated

---
+++ app/api/views.py
@@ -0,0 +1,26 @@
+"""
+API views.
+"""
+from flask import jsonify
+
+from app.utils import TransportAPIWrapper
+from . import api_blueprint
+
+# Lviv public transport API wrapper object
+transport = TransportAPIWrapper()
+
+
+@api_blueprint.route('/stops')
+def stops_api():
+    stops = transport.get_all_stops()
+    return jsonify(count=len(stops), stops=stops)
+
+
+@api_blueprint.route('/routes')
+def routes_api():
+    routes = transport.get_all_routes()
+    return jsonify(count=len(routes), routes=routes)
+
+# @app.route('/err')
+# def test_err():
+#     return jsonify(error=404), 410

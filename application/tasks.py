abf27e69fdc68334a46b6da44d9e322758449e0a
fastfuse <banderstadt251@gmail.com>
2018-11-29 22:15:26 +0200

    updates and fixed; celery beat added

--- application/tasks.py
+++ application/tasks.py
@@ -2,10 +2,15 @@
 Celery tasks.
 """
 import json
+import logging
+from time import sleep
 
 from paho.mqtt.publish import single
+from sqlalchemy.exc import OperationalError
 
-from application import celery, utils, app
+from application import celery, utils, app, models
+
+log = logging.getLogger('Publisher')
 
 # Lviv public transport API wrapper object
 transport = utils.TransportAPIWrapper()
@@ -31,3 +36,32 @@ def get_stop_info(stop_code):
     single(hostname=broker,
            topic=f"lwo/transport/stop/{stop_code}",
            payload=json.dumps(data), retain=True)
+
+
+@celery.task
+def publish_stops_info():
+    try:
+        log.info('Start...')
+
+        stops = models.Stop.query.all()
+
+        log.info(f"Found {len(stops)} stops")
+
+        stops_codes = [stop.code for stop in stops]
+
+        for code in stops_codes:
+            log.info(f"Getting info for stop code {code}")
+            get_stop_info.delay(code)
+
+        log.info('Sleep...')
+        sleep(30)
+
+    except OperationalError as e:
+        log.warning('Could not connect to PSQL. Sleep...')
+        sleep(5)
+
+
+@celery.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    # Call update_vehicle_position() every 10 seconds
+    sender.add_periodic_task(30.0, publish_stops_info.s(), name='update stops information')
--- publisher.py
+++ publisher.py
@@ -11,58 +11,58 @@ from paho.mqtt.client import Client
 
 if __name__ == '__main__':
 
-    # # The callback for when the client receives a CONNACK response from the server.
-    # def on_connect(client, userdata, flags, rc):
-    #     print("Connected with result code " + str(rc))
-    #
-    #     # Subscribing in on_connect() means that if we lose the connection and
-    #     # reconnect then subscriptions will be renewed.
-    #     client.subscribe("lwo/transport/stop/*")
-    #
-    #
-    # def on_subscribe(client, userdata, mid, granted_qos):
-    #     print(f'subscribed to topic: {mid}')
-    #
-    #
-    # # The callback for when a PUBLISH message is received from the server.
-    # def on_message(client, userdata, msg):
-    #     print(msg.topic + " " + str(msg.payload))
-    #
-    #
-    # client = Client()
-    # client.on_connect = on_connect
-    # client.on_message = on_message
-    # client.on_subscribe = on_subscribe
-    #
-    # client.connect("test.mosquitto.org")
-    # # client.connect("iot.eclipse.org")
-    # # client.connect("broker.hivemq.com")
-    #
-    # # Blocking call that processes network traffic, dispatches callbacks and
-    # # handles reconnecting.
-    # # Other loop*() functions are available that give a threaded interface and a
-    # # manual interface.
-    # client.loop_forever()
+    # The callback for when the client receives a CONNACK response from the server.
+    def on_connect(client, userdata, flags, rc):
+        print("Connected with result code " + str(rc))
 
-    while True:
-        try:
-            log.info('Start...')
+        # Subscribing in on_connect() means that if we lose the connection and
+        # reconnect then subscriptions will be renewed.
+        client.subscribe("lwo/transport/stop/+")
 
-            stops = models.Stop.query.all()
 
-            log.info(f"Found {len(stops)} stops")
+    def on_subscribe(client, userdata, mid, granted_qos):
+        print(f'subscribed to topic: {mid}')
 
-            stops_codes = [stop.code for stop in stops]
 
-            for code in stops_codes:
-                log.info(f"Getting info for stop code {code}")
-                tasks.get_stop_info.delay(code)
+    # The callback for when a PUBLISH message is received from the server.
+    def on_message(client, userdata, msg):
+        print(msg.topic + " " + str(msg.payload))
 
-            log.info('Sleep...')
-            sleep(30)
 
-        except OperationalError as e:
-            log.warning('Could not connect to PSQL. Sleep...')
-            sleep(5)
+    client = Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.on_subscribe = on_subscribe
+
+    client.connect("test.mosquitto.org")
+    # client.connect("iot.eclipse.org")
+    # client.connect("broker.hivemq.com")
+
+    # Blocking call that processes network traffic, dispatches callbacks and
+    # handles reconnecting.
+    # Other loop*() functions are available that give a threaded interface and a
+    # manual interface.
+    client.loop_forever()
+
+    # while True:
+    #     try:
+    #         log.info('Start...')
+    #
+    #         stops = models.Stop.query.all()
+    #
+    #         log.info(f"Found {len(stops)} stops")
+    #
+    #         stops_codes = [stop.code for stop in stops]
+    #
+    #         for code in stops_codes:
+    #             log.info(f"Getting info for stop code {code}")
+    #             tasks.get_stop_info.delay(code)
+    #
+    #         log.info('Sleep...')
+    #         sleep(30)
+    #
+    #     except OperationalError as e:
+    #         log.warning('Could not connect to PSQL. Sleep...')
+    #         sleep(5)
 
 # TODO: switch to celery beat
